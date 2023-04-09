53170804a7fde03ffcb12050d72859b94a3b21e6
Aleksey Andrianov <a.a.andrianov@tinkoff.ru>
2023-04-09 22:50:55 +0400

    2-js-dom

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const table = document.getElementsByTagName("table");
+const button = document.getElementById('button');
 
-console.log(table);
+button.addEventListener('click', () => {
+   const rows = document.getElementsByClassName('row');
+   const evenness = rows.length % 2 ? 'even' : 'odd';
+   const table = document.getElementsByTagName('tbody')[0];
+   const newRow = document.createElement('tr');
+
+   newRow.className = `row ${evenness}`;
+   newRow.innerHTML = `<td>${randomString()}</td><td>${randomString()}</td>`;
+
+   table.appendChild(newRow);
+});
+
+function randomString() {
+    return (Math.random() + 1).toString(36).substring(7);
+}
