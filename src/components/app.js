b725b0b4a511dec7f09705b091b1d4a0ba9c8b69
Aleksey Andrianov <a.a.andrianov@tinkoff.ru>
2023-04-12 22:02:13 +0400

    5-rest

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,8 @@ import {Element} from './element';
 import {Form} from "./form";
 import {List} from "./list";
 
+const API = 'http://90.156.228.208:8080/api/raccoon';
+
 export class App extends Element {
     form;
     list;
@@ -17,9 +19,24 @@ export class App extends Element {
         this.element.className = 'application';
         this.form = new Form(this.element, this.formSubmit.bind(this));
         this.list = new List(this.element);
+        this.reload();
     }
 
-    formSubmit(value) {
-        console.log(value);
+    formSubmit = (value) => {
+        fetch(API, {
+            method: 'POST',
+            body: JSON.stringify(value),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => this.reload());
+    }
+
+    reload = () => {
+        return fetch(API, {
+            method: 'GET',
+        })
+            .then(res => res.json())
+            .then(res => this.list.setRows(res));
     }
 }
--- src/components/list.js
+++ src/components/list.js
@@ -1,9 +1,26 @@
 import {Element} from "./element";
 
 export class List extends Element {
+    itemsWrapper;
     constructor(parentElement) {
         super(parentElement);
 
         this.element.className = 'list';
+
+        const header = document.createElement('div');
+        header.className = 'header';
+        header.innerHTML = `<div class="row"><div class="name">Имя</div><div class="description">Описание</div></div>`;
+        this.itemsWrapper = document.createElement('div');
+        this.itemsWrapper.className = 'rows';
+
+        this.element.appendChild(header);
+        this.element.appendChild(this.itemsWrapper);
+    }
+
+    setRows(rows) {
+        this.itemsWrapper.innerHTML = '';
+        this.itemsWrapper.innerHTML = rows.map(row => {
+            return `<div class="row"><div class="name">${row.name}</div><div class="description">${row.description}</div></div>`;
+        }).join('');
     }
 }
